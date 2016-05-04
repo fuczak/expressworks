8712c8d02155ee6817f54e35768a6746d004fb53
Damian <prus.damian@gmail.com>
2016-05-04 14:37:34 +0200

    #1 Add test

--- exercise8.js
+++ exercise8.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var fs = require('fs')
 var app = express()
-
+// test
 app.get('/books', function (req, res) {
 	fs.readFile(process.argv[3], function (err, content) {
 		if(err)
